a5c89b0b350a2d89dce96d74bc6ec749dabaf2bf
Jian yong Lei <jianyonglei@Jians-MacBook-Pro.local>
2022-11-30 17:12:44 -0500

    created Note and User models

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3030'
+    'http://localhost:3000',
+    'https://jianylei.com',
+    'https://www.jianylei.com'
 ]
 
 module.exports = allowedOrigins;
